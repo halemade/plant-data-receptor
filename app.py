998e25e3471e2e8893b1a9680d2ff935f4247d5c
plant-data-receptor.workspace <plant-data-receptor.workspace>
2022-05-04 22:05:59 +0000

    add print statement to output logs

--- app.py
+++ app.py
@@ -10,19 +10,9 @@ def hello_world():
 
 @server.route('/readings/<datetime>/<primary_sensor_id>/<reading_value>', methods=['POST'])
 def return_data(datetime, primary_sensor_id, reading_value):
-    return {'datetime':datetime, "id": primary_sensor_id, "reading_value":reading_value}
-
-# app = Flask(__name__)
-# api = Api(server)
-
-# class SensorReading(Resource):
-#     def post(self, datetime, primary_sensor_id, reading_value):
-#         self.reading_value = reading_value
-#         self.datetime = datetime
-#         self.primary_sensor_id = primary_sensor_id
-#         return {'datetime':self.datetime, "id": self.primary_sensor_id, "reading_value": self.reading_value}
-
-# api.add_resource(SensorReading, "/readings/<string:datetime>/<string:primary_sensor_id>/<float:reading_value>")    
+    reading = {'datetime':datetime, "id": primary_sensor_id, "reading_value":reading_value} 
+    print(reading)
+    return reading
 
 if __name__ == '__main__':
     server.run(debug=True, host="0.0.0.0", port=8050)
--- tests.py
+++ tests.py
@@ -1,20 +1,20 @@
 import requests
 
-BASE = "https://dash-customer-success.plotly.host/plant-data-receptor/"
+workspace_base = "http://172.17.0.67:8050"
+deployed_base = ""
 
 sensor_endpoint = "/readings"
 datetime = "20220502T01:01:00"
 sensor_id = "sensor_id_string"
 reading_value =3.26
 
-payload = {'csrf_token': 'dEjS0pYL6yq8CnMieQQ3p2s3VDR5hWWX', 'has_password': True, 'username': 'taylor','is_active': True}
 
 session = requests.Session()
 session.auth = ('taylor', 'dataliberation')
 
 auth = session.post(BASE)
-# response = requests.post(BASE+f"readings/{datetime}/{sensor_id}/{reading_value}")
-response = requests.post(BASE+f"readings/{datetime}", auth=('taylor','dataliberation'))
+response = requests.post(workspace_base+f"/readings/{datetime}/{sensor_id}/{reading_value}")
+
 
 if response.status_code != 200:
     print(response.status_code)
