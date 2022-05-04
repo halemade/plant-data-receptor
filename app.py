f5f3a287b5c40a39e1d135e07b519cca7f3bce79
plant-data-receptor.workspace <plant-data-receptor.workspace>
2022-05-04 21:25:00 +0000

    update app

--- app.py
+++ app.py
@@ -3,21 +3,17 @@ from flask_restful import Api, Resource
 
 server = Flask(__name__)
 
-# @server.route('/')
-# def hello_world():
-#     return 'Hello, world!'
+@server.route('/')
+def hello_world():
+    return 'Hello, world!'
 
 
-# @server.route('/data')
-# def return_data():
-#     return flask.jsonify({
-#         'datetime': [1, 2, 3, 4, 5],
-#         'reading_value': [3, 1, 2, 3, 4],
-#         'sensor_name':'ficus_lyrata'
-#     })
+@server.route('/readings/<string:datetime>/<string:primary_sensor_id>/<float:reading_value>"')
+def return_data(datetime, primary_sensor_id, reading_value):
+    return {'datetime':datetime, "id": primary_sensor_id, "reading_value":reading_value}
 
-app = Flask(__name__)
-api = Api(server)
+# app = Flask(__name__)
+# api = Api(server)
 
 # class SensorReading(Resource):
 #     def post(self, datetime, primary_sensor_id, reading_value):
@@ -26,13 +22,7 @@ api = Api(server)
 #         self.primary_sensor_id = primary_sensor_id
 #         return {'datetime':self.datetime, "id": self.primary_sensor_id, "reading_value": self.reading_value}
 
-# api.add_resource(SensorReading, "/readings/<string:datetime>/<string:primary_sensor_id>/<float:reading_value>")   
-# 
-class SensorReading(Resource):
-    def post(self, reading_value):
-        return {'reading':reading_value}
-
-api.add_resource(SensorReading, "/readings/<string:reading_value>")    
+# api.add_resource(SensorReading, "/readings/<string:datetime>/<string:primary_sensor_id>/<float:reading_value>")    
 
 if __name__ == '__main__':
     server.run(debug=True, host="0.0.0.0", port=8050)
--- tests.py
+++ tests.py
@@ -7,7 +7,16 @@ datetime = "20220502T01:01:00"
 sensor_id = "sensor_id_string"
 reading_value =3.26
 
-# response = requests.post(BASE+f"readings/{datetime}/{sensor_id}/{reading_value}")
-response = requests.post(BASE+f"readings/{datetime}")
+payload = {'csrf_token': 'dEjS0pYL6yq8CnMieQQ3p2s3VDR5hWWX', 'has_password': True, 'username': 'taylor','is_active': True}
 
-print(response.json())
+session = requests.Session()
+session.auth = ('taylor', 'dataliberation')
+
+auth = session.post(BASE)
+# response = requests.post(BASE+f"readings/{datetime}/{sensor_id}/{reading_value}")
+response = requests.post(BASE+f"readings/{datetime}", auth=('taylor','dataliberation'))
+
+if response.status_code != 200:
+    print(response.status_code)
+else:
+    print(response.json())
